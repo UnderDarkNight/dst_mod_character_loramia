2f6f3eb999b8e076f919da222fce353e88a8ab79
unknown <under.dark.night@outlook.com>
2024-08-16 19:03:11 +0800

    sleepingbag and hunger value bug fix

--- key_modules_for_loramia/00_others/__all_others_init.lua
+++ key_modules_for_loramia/00_others/__all_others_init.lua
@@ -14,3 +14,6 @@ modimport("key_modules_for_loramia/00_others/03_equipment_slot.lua")
 
 modimport("key_modules_for_loramia/00_others/04_rpc_event_register.lua")
 --- RPC 信道注册
+
+modimport("key_modules_for_loramia/00_others/05_sleepingbag_and_hunger.lua")
+--- 帐篷和饥饿值
